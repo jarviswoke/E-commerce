40c58ee061f45cce508c923530d1739f733ed88b
Sachi <202452318@iiitvadodara.ac.in>
2026-01-05 23:29:21 +0530

    frontend initial

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './App.css';
+import Home from "./pages/Home"; 
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <>
+        <BrowserRouter>
+            <Routes>
+              <Route path="/" element = {<Layout/>}>
+                <Route index element = {<Home/>} />
+              </Route>
+            </Routes>
+        </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
