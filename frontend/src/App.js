46329505de2bceb46ca4a58f2e16aa172dbba6a4
Sachi <202452318@iiitvadodara.ac.in>
2026-01-07 01:13:47 +0530

    header updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './App.css';
 import Home from "./pages/Home"; 
+import Contact from "./pages/Contact"; 
+import About from "./pages/About"; 
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Layout from "./components/Layout";
 
@@ -11,6 +13,8 @@ function App() {
             <Routes>
               <Route path="/" element = {<Layout/>}>
                 <Route index element = {<Home/>} />
+                <Route path="about" element = {<About/>} />
+                <Route path="contact" element = {<Contact/>} />
               </Route>
             </Routes>
         </BrowserRouter>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <>
+      <header className="bg-dark py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping Over $100 & Free Returns
+              </p>
+            </div>
+            <div className="col-6 text-end">
+              <p className="text-white mb-0">
+                Helpline:{" "}
+                <a href="tel:+919763844464"
+                  className="text-white text-decoration-none">
+                  +91 9763844464
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+      <header className="header-upper py-3">
+        <div className="row"></div>
+          <div className="col-2"></div>
+          <div className="col-3"></div>
+      </header>
+    </>
+  );
+};
 
-export default Header
+export default Header;
